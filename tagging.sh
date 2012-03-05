7a1330790f648199c964504c059582374a635fe1
Philipp Klose <me@thehippo.de>
2012-03-06 00:46:33 +0100

    added helperscripts for tag creation

---
+++ tagging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# lets do some bash magic, create an array of all existing tags
+declare -a tags=(`git tag`);
+# find all remote tags and strip down their names
+git for-each-ref --format='%(refname)' refs/remotes/tags/ |
+cut -d / -f 4 |
+while read tag
+do
+	# do more bash magic, the next two line check if the tag already
+	# exists in git
+	echo "${tags[@]}" | grep -qv "$tag"
+	if [ $? -eq 0 ]; then
+		git tag "$tag" "refs/remotes/tags/$tag" -m $tag;
+	fi
+done
